abb2024b0bb024c912ad128f957ee784cb9864bc
AYushgupta4387 <ayushgupta4387@gmail.com>
2022-02-05 22:51:19 +0530

    first commit of my blog, complete fundamentals of JS

---
+++ js/app.js
@@ -0,0 +1,77 @@
+"use strict";
+
+let clickedSection;
+
+const changeColorOfSection = function (sectionNumber) {
+  clickedSection = document.querySelector(`.section-btn-${sectionNumber}`);
+  if ((clickedSection.style.backgroundColor = "#393e46")) {
+    document.querySelector(".section-btn-1").style.backgroundColor = "#393e46";
+    document.querySelector(".section-btn-2").style.backgroundColor = "#393e46";
+    document.querySelector(".section-btn-3").style.backgroundColor = "#393e46";
+    document.querySelector(".section-btn-4").style.backgroundColor = "#393e46";
+    document.querySelector(".section-btn-5").style.backgroundColor = "#393e46";
+    document.querySelector(".section-btn-6").style.backgroundColor = "#393e46";
+    document.querySelector(".section-btn-7").style.backgroundColor = "#393e46";
+
+    clickedSection.style.backgroundColor = "#222831";
+  }
+};
+
+const showClickedSection = function (sectionNumber) {
+  if (
+    document
+      .querySelector(`.section-${sectionNumber}`)
+      .classList.contains("hidden")
+  ) {
+    if (sectionNumber == 1) {
+      for (let i = 0; i < 7; i++) {
+        document
+          .querySelectorAll(".content-section")
+          [i].classList.add("hidden");
+      }
+      document.querySelector(`.section-1`).classList.remove("hidden");
+    } else if (sectionNumber == 2) {
+      for (let i = 0; i < 7; i++) {
+        document
+          .querySelectorAll(".content-section")
+          [i].classList.add("hidden");
+      }
+      document.querySelector(`.section-2`).classList.remove("hidden");
+    } else if (sectionNumber == 3) {
+      for (let i = 0; i < 7; i++) {
+        document
+          .querySelectorAll(".content-section")
+          [i].classList.add("hidden");
+      }
+      document.querySelector(`.section-3`).classList.remove("hidden");
+    } else if (sectionNumber == 4) {
+      for (let i = 0; i < 7; i++) {
+        document
+          .querySelectorAll(".content-section")
+          [i].classList.add("hidden");
+      }
+      document.querySelector(`.section-4`).classList.remove("hidden");
+    } else if (sectionNumber == 5) {
+      for (let i = 0; i < 7; i++) {
+        document
+          .querySelectorAll(".content-section")
+          [i].classList.add("hidden");
+      }
+      document.querySelector(`.section-5`).classList.remove("hidden");
+    } else if (sectionNumber == 6) {
+      for (let i = 0; i < 7; i++) {
+        document
+          .querySelectorAll(".content-section")
+          [i].classList.add("hidden");
+      }
+      document.querySelector(`.section-6`).classList.remove("hidden");
+    } else if (sectionNumber == 7) {
+      for (let i = 0; i < 7; i++) {
+        document
+          .querySelectorAll(".content-section")
+          [i].classList.add("hidden");
+      }
+      document.querySelector(`.section-7`).classList.remove("hidden");
+    }
+  }
+};
