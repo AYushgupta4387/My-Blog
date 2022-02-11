f3dcd78e9df9037cc74b91de7a633e8248ed99b8
AYushgupta4387 <ayushgupta4387@gmail.com>
2022-02-11 18:08:10 +0530

    incomplete implement of responsive view

--- js/app.js
+++ js/app.js
@@ -75,3 +75,29 @@ const showClickedSection = function (sectionNumber) {
     }
   }
 };
+
+// SIDE-BAR FUNCTIONALITY
+const toggleBtn = document.querySelector(".header__bars-btn");
+const sidebar = document.querySelector(".side-bar");
+
+const toggleBtn_2 = document.querySelector(".sb-bars");
+
+toggleBtn.addEventListener("click", function () {
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+    sidebar.classList.add("side-bar-invisible");
+  } else {
+    sidebar.classList.add("show-sidebar");
+    sidebar.classList.remove("side-bar-invisible");
+  }
+});
+
+toggleBtn_2.addEventListener("click", function () {
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+    sidebar.classList.add("side-bar-invisible");
+  } else {
+    sidebar.classList.add("show-sidebar");
+    sidebar.classList.remove("side-bar-invisible");
+  }
+});
